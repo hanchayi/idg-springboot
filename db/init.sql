7b4b82096f3d4eecf08a55175835fbac6cb9d3af
hanchayi <hanchayi@163.com>
2023-03-21 17:01:50 +0800

    feat: sql

---
+++ db/init.sql
@@ -0,0 +1,18 @@
+-- Adminer 4.8.1 MySQL 8.0.32 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- 2023-03-21 09:01:26
