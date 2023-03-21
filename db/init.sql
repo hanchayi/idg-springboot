72703cb866948dd71cd90bd494daf7efa9a03976
hanchayi <hanchayi@163.com>
2023-03-21 19:35:05 +0800

    feat: sql

--- db/init.sql
+++ db/init.sql
@@ -7,6 +7,25 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
+DROP TABLE IF EXISTS `demo_score`;
+CREATE TABLE `demo_score` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `demo_student_id` int NOT NULL,
+  `score` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `demo_student_id` (`demo_student_id`),
+  CONSTRAINT `demo_score_ibfk_1` FOREIGN KEY (`demo_student_id`) REFERENCES `demo_student` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+DROP TABLE IF EXISTS `demo_student`;
+CREATE TABLE `demo_student` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -15,4 +34,4 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
--- 2023-03-21 09:01:26
+-- 2023-03-21 11:34:42
