686ce31081d4d0611af3c4dcc2aef0687c617626
hanchayi <hanchayi@163.com>
2023-03-22 13:55:53 +0800

    feat: demo student get by id

--- src/main/java/com/idg/demo/controller/DemoStudentController.java
+++ src/main/java/com/idg/demo/controller/DemoStudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,11 @@ public class DemoStudentController {
 	@GetMapping("/demo-students")
 	public List<DemoStudent> getDemoStudents() {
        return userDemoStudent.list();
+	}
+
+	@GetMapping("/demo-students/{id}")
+	public DemoStudent getDemoStudentById(@PathVariable Integer id) {
+       return userDemoStudent.getById(id);
 	}
 
     @PostMapping("/demo-students")
