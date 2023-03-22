d5c80e83b7a723c9872d0394d76afbd5d0efea39
hanchayi <hanchayi@163.com>
2023-03-22 15:52:44 +0800

    feat: delete add update

--- src/main/java/com/idg/demo/controller/DemoStudentController.java
+++ src/main/java/com/idg/demo/controller/DemoStudentController.java
@@ -3,9 +3,11 @@ package com.idg.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,9 +30,21 @@ public class DemoStudentController {
        return userDemoStudent.getById(id);
 	}
 
+	@DeleteMapping("/demo-students/{id}")
+	public boolean deleteDemoStudentById(@PathVariable Integer id) {
+       return userDemoStudent.removeById(id);
+	}
+
     @PostMapping("/demo-students")
-	public boolean postDemoStudents(@RequestParam("name") String name) {
+	public boolean addDemoStudent(@RequestParam("name") String name) {
         System.out.println("name:" + name);
        return userDemoStudent.save(new DemoStudent(name));
+	}
+
+	@PutMapping("/demo-students/{id}")
+	public boolean updateDemoStudent(@PathVariable Integer id, @RequestParam("name") String name) {
+		DemoStudent student = new DemoStudent(name);
+		student.setId(id);
+       return userDemoStudent.updateById(student);
 	}
 }
