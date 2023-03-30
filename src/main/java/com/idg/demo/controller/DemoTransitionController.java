0a67f4d864ba502f91128e1d304c55cfb18e1fd7
hanchayi <hanchayi@163.com>
2023-03-30 15:56:02 +0800

    feat: demo

--- src/main/java/com/idg/demo/controller/DemoTransitionController.java
+++ src/main/java/com/idg/demo/controller/DemoTransitionController.java
@@ -1,7 +1,10 @@
 package com.idg.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.idg.demo.service.IDemoTransitionService;
 
 @RestController
@@ -15,4 +18,11 @@ public class DemoTransitionController {
     	demoTransitionService.test();
 		return true;
 	}
+
+	@PostMapping("upload")
+    public String uploadFile(MultipartFile file){
+      //todo upload
+      String previewPath = "";
+      return previewPath;
+    }
 }
